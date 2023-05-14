2a2e4cf3f1f4ed33c3f3c3cc38b69b23cdffc06e
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-05-14 23:29:17 +0530

    Added: Rate Limiting using resilience4j

--- src/main/java/com/heliversetask/Controller/QuizController.java
+++ src/main/java/com/heliversetask/Controller/QuizController.java
@@ -3,13 +3,14 @@ package com.heliversetask.Controller;
 import com.heliversetask.Payloads.ApiResponse;
 import com.heliversetask.Payloads.QuizDto;
 import com.heliversetask.Service.QuizService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/quizzes")
@@ -19,6 +20,7 @@ public class QuizController {
 
     //Create a quiz
     @PostMapping()
+    @RateLimiter(name="quizCreateService", fallbackMethod = "rateLimiterFallback")
     ResponseEntity<?> createQuiz(@Valid @RequestBody QuizDto quizDto){
         if (quizDto.getStartDate().compareTo(quizDto.getEndDate()) > 0) {
             return ResponseEntity.status(BAD_REQUEST).body(new ApiResponse("Start date must not be greater than end date!!", false));
@@ -28,19 +30,27 @@ public class QuizController {
 
     //Get all active quizzes
     @GetMapping("/active")
+    @RateLimiter(name="default", fallbackMethod = "rateLimiterFallback")
     ResponseEntity<?> getAllActiveQuiz() {
         return ResponseEntity.status(OK).body(this.quizService.getActiveQuizzes());
     }
 
     //Get the quiz result if quiz ended
     @GetMapping("/{quizId}/result")
+    @RateLimiter(name="default", fallbackMethod = "rateLimiterFallback")
     ResponseEntity<?> getQuizResult(@PathVariable("quizId") Long quizId){
         return this.quizService.getQuizResult(quizId);
     }
 
     //Get all the quiz
     @GetMapping("/all")
+    @RateLimiter(name="default", fallbackMethod = "rateLimiterFallback")
     ResponseEntity<?> getAllQuizzes(){
         return ResponseEntity.status(OK).body(this.quizService.getAllQuizzes());
+    }
+
+    //Fallback method to handle rate-limiting response
+    public ResponseEntity<?> rateLimiterFallback(Exception e){
+        return ResponseEntity.status(TOO_MANY_REQUESTS).body(new ApiResponse("Limit Reached, further calls are not permitted, please try after some time!!", false));
     }
 }
