0d19b1ff366a48978641ba19b846a36ffd52ae2b
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-05-14 10:46:03 +0530

    Initial Commit

---
+++ src/main/java/com/heliversetask/HeliverseTaskApplication.java
@@ -0,0 +1,13 @@
+package com.heliversetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliverseTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliverseTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heliversetask/HeliverseTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliversetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliverseTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
