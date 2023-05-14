189a0c4b6017902755c630269949caa2e0579d4d
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-05-14 14:34:33 +0530

    Added: Scheduler with cron

--- src/main/java/com/heliversetask/Repository/QuizRepo.java
+++ src/main/java/com/heliversetask/Repository/QuizRepo.java
@@ -2,8 +2,15 @@ package com.heliversetask.Repository;
 
 import com.heliversetask.Models.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Repository
 public interface QuizRepo extends JpaRepository<Quiz, Long> {
+    @Query("SELECT q FROM Quiz q WHERE q.status <> 'finished' AND (q.startDate <= :now OR q.endDate <= :now)")
+    List<Quiz> findAllQuizzesToUpdateStatus(@Param("now") LocalDateTime now);
 }
--- src/main/java/com/heliversetask/Service/Impl/QuizServiceImpl.java
+++ src/main/java/com/heliversetask/Service/Impl/QuizServiceImpl.java
@@ -7,7 +7,10 @@ import com.heliversetask.Repository.QuizRepo;
 import com.heliversetask.Service.QuizService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +27,22 @@ public class QuizServiceImpl implements QuizService {
         }
         this.quizRepo.save(quiz);
         return quiz;
+    }
+
+    //This run every minute
+    @Scheduled(cron = "0 * * * * *")
+    public void updateQuizStatus() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Quiz> quizzesToUpdate = this.quizRepo.findAllQuizzesToUpdateStatus(now);
+        for (Quiz quiz : quizzesToUpdate) {
+            if (quiz.getStartDate().isAfter(now)) {
+                quiz.setStatus("inactive");
+            } else if (quiz.getEndDate().isBefore(now)) {
+                quiz.setStatus("finished");
+            } else {
+                quiz.setStatus("active");
+            }
+            this.quizRepo.save(quiz);
+        }
     }
 }
